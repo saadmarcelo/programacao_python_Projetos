7e59eddd08bf4fda3c6d3f333192e217594df050
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-18 23:03:06 -0400

    secao 4+[20-22]

---
+++ secao_4/20_metodos_strings/main.py
@@ -0,0 +1,11 @@
+mensagem = "Eu adoro comida Caseira"
+
+print(mensagem)
+print(mensagem.lower())
+print(mensagem.upper())
+print(mensagem.capitalize())
+print(mensagem.find("C"))
+print(mensagem.find("adoro"))
+print(mensagem.replace("Caseira", "feita em casa"))
+print(mensagem.strip())
+print(mensagem.swapcase())
---
+++ secao_4/21_operacoes_mat/main.py
@@ -0,0 +1,9 @@
+calculo = 2 + 2 * 3
+
+print(calculo)
+
+
+# Parenteses
+# Exponenciais
+# Multi e Divisao
+# Adicao e Substracao
