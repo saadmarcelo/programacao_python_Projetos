d565fba0fa6096081936341b25691a29ebd59cbb
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-20 13:56:06 -0400

    for loops

---
+++ secao_6/30_nasted_loop/main.py
@@ -0,0 +1,10 @@
+for numero1 in range(5):
+    print(numero1)
+    for numero2 in range(5):
+        print(numero2)
+
+
+for num1 in range(1, 6):
+    print("Produto " + str(num1))
+    for numero2 in range(5):
+        print(numero2)
---
+++ secao_6/31_separando_strings/main.py
@@ -0,0 +1,4 @@
+palavra = "FANTASTICO"
+
+for espaco in palavra:
+    print(f" {espaco}", end="")
