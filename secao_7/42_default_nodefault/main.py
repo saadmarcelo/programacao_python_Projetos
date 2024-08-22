ffc588c7b78ccf930dca2087849aa337df876046
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-22 12:44:18 -0400

    secao 7 , funcoes

---
+++ secao_7/42_default_nodefault/main.py
@@ -0,0 +1,6 @@
+def boas_vindas(nome, quantidade=6):
+    print(f"olá {nome}.")
+    print(f"Temos {str(quantidade)} laptops em estoque")
+
+
+boas_vindas("Marcos", 4)
---
+++ secao_7/43_print_return/main.py
@@ -0,0 +1,10 @@
+def cliente1(nome):
+    print(f"Olá {nome}")
+
+
+def cliente2(nome):
+    return f"Olá {nome}"
+
+
+cliente1("Maria")
+print(cliente2("José"))
