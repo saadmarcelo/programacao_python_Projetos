63ebaac5b10201c0519d5ff12fb31c87d7144bcb
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-23 00:27:44 -0400

    secao 7 import modulo

---
+++ secao_7/45_args_nomeando_paramentros/main.py
@@ -0,0 +1,7 @@
+def agencia(**carro):
+    return carro
+
+
+print(agencia(marca="Gol", cor="Branca", motor=1.0))
+print(agencia(marca="Honda", cor="prata", motor=2.0))
+print(agencia(marca="bmw", cor="Azul", motor=2.0))
