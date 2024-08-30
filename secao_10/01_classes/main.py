0da198abffaa386c9abb0d28891671f42ff04730
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-29 22:47:53 -0400

    secao 10, classes

---
+++ secao_10/01_classes/main.py
@@ -0,0 +1,4 @@
+nome = "Marcelo"
+nome_lower = nome.lower()
+
+print(nome_lower)
---
+++ secao_10/02_primeira_classe/main.py
@@ -0,0 +1,10 @@
+class Funcionarios:
+    nome = "Elena"
+    sobrenome = "Cabral"
+    data_nascimento = "12/01/2009"
+
+
+usuario1 = Funcionarios()
+print(usuario1.nome)
+print(usuario1.sobrenome)
+print(usuario1.data_nascimento)
