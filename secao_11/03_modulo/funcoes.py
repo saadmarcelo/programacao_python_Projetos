70079825d068afc097541651aa535f11db0fdd83
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-01 12:56:31 -0400

    secao 11 modulos

---
+++ secao_11/03_modulo/funcoes.py
@@ -0,0 +1,13 @@
+def somar():
+    print("Esta funcao vai somar valores")
+
+
+def multi():
+    print("Esta funcao vai multiplicar valores")
+
+
+def find_index(to_find, item):
+    for i, valor in enumerate(to_find):
+        if valor == item:
+            return i
+    return -1
---
+++ secao_11/03_modulo/main.py
@@ -0,0 +1,6 @@
+from funcoes import find_index
+
+list1 = ["a", "b", "c", "d", "e"]
+
+var1 = find_index(list1, "t")
+print(var1)
