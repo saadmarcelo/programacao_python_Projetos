a01bb2fb90b27b5d320dac3217434eccff217a36
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-02 23:09:00 -0400

    desafios 11 ate 14

---
+++ secao_13_DESAFIOS/12_nested_for_loop/main.py
@@ -0,0 +1,6 @@
+frutas = ["banana", "maçã", "laranja"]
+vegetais = ["alface", "tomate", "cenoura"]
+
+for f in frutas:
+    for v in vegetais:
+        print(f + " " + v)
---
+++ secao_13_DESAFIOS/14_loop_break/main.py
@@ -0,0 +1,12 @@
+num = 1
+
+while num <= 10:
+    if num > 5:
+        break
+    print(num)
+    num += 1
+
+for y in range(1, 11):
+    if y == 5:
+        continue
+    print(y)
