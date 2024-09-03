4b02002b50385042a6cbd42080f2ee696631f8f9
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-03 10:37:37 -0400

    desafios 22

--- secao_13_DESAFIOS/22_pais_capital/main.py
+++ secao_13_DESAFIOS/22_pais_capital/main.py
@@ -1,4 +1,4 @@
-x = {
+cidades = {
     "brasil": "brasilia",
     "argentina": "buenos aires",
     "franca": "paris",
@@ -7,7 +7,7 @@ x = {
 
 pesquisa_capital = input("Entre com o nome do pais que deseja pesquisar: ")
 
-if pesquisa_capital in x:
-    print(f"A capital de {pesquisa_capital} é {x[pesquisa_capital]} ")
+if pesquisa_capital in cidades:
+    print(f"A capital de {pesquisa_capital} é {cidades[pesquisa_capital]} ")
 else:
     print(f"A capital {pesquisa_capital} não esta na lista")
